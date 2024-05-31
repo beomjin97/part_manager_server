c78ab42e7c0cf393a7bca9760c09077d0f854faf
Beomjin <qjawls646@gmail.com>
2024-05-31 07:52:58 +0000

    resolve controller's dependency

--- src/parts/parts.controller.spec.ts
+++ src/parts/parts.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PartsController } from './parts.controller';
+import { PartsService } from './parts.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Part } from './entities/part.entity';
 
 describe('PartsController', () => {
   let controller: PartsController;
@@ -7,6 +11,12 @@ describe('PartsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PartsController],
+      providers: [
+        PartsService, 
+        {
+        provide: getRepositoryToken(Part),
+        useClass: Repository
+        }]
     }).compile();
 
     controller = module.get<PartsController>(PartsController);
