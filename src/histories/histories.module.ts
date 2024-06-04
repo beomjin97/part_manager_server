a0132d2bebf621ac338570c057bd5b04a3c542d5
beomjin97 <qjawls646@gmail.com>
2024-06-04 16:40:12 +0900

    History entity import

--- src/histories/histories.module.ts
+++ src/histories/histories.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HistoriesService } from './histories.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { History } from './entities/history.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([History])],
