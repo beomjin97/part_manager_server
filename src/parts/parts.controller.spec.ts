934c73f11eacddecd9fcdb3754b0257d43e42d38
Beomjin <qjawls646@gmail.com>
2024-06-04 05:10:38 +0000

    controller 기본 로직 작성 및 테스트 코드 작성

--- src/parts/parts.controller.spec.ts
+++ src/parts/parts.controller.spec.ts
@@ -7,6 +7,8 @@ import { Part } from './entities/part.entity';
 
 describe('PartsController', () => {
   let controller: PartsController;
+  let service: PartsService;
+  let part: Part 
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,10 +21,41 @@ describe('PartsController', () => {
         }]
     }).compile();
 
+    part = {
+      id: 1,
+      number: "test_part_number",
+      name: "test_part_name", 
+      manufacturer: "test_manufacturer", 
+      storageLocation: "test_storage_location", 
+      detailedStorageLocation: "testDetailedStorageLocation", 
+      histories: [{
+        id: 1,
+        isImport: true,
+        date: new Date(),
+        quantity: 1,
+        part: new Part()
+      }]
+    }
+
     controller = module.get<PartsController>(PartsController);
+    service = module.get<PartsService>(PartsService);
+
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAll', async () => {
+      const spy = jest
+        .spyOn(service, 'findAll')
+        .mockImplementation(():any => Promise.resolve([part]))
+
+      expect(await controller.findAll()).toEqual([part]);
+      expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  it('create', async () => {
+    const spy = jest.spyOn(service, 'save').mockImplementation(())
+  })
 });
--- src/parts/parts.controller.ts
+++ src/parts/parts.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { PartsService } from './parts.service';
+import { SavePartDto } from './dtos/savePart.dto';
+import { UpdatePartDto } from './dtos/updatePart.dto';
+import { SaveHistoryDto } from 'src/histories/dtos/saveHistory.dto';
+import { UpdateHistoryDto } from 'src/histories/dtos/updateHistory.dto';
 
-@Controller('parts')
+@Controller('part')
 export class PartsController {
     
-    constructor(private partsService:PartsService ) {}
+    constructor(private partsService:PartsService) {}
     
     @Get()
     async findAll() {
@@ -13,10 +17,41 @@ export class PartsController {
 
     @Get("/:id")
     async findOne(@Param("id") id: string) {
-        const part =  await this.partsService.findOneById(parseInt(id));
-        if (!part) {
-            throw new NotFoundException();
-        }
-        return part;
+        return await this.partsService.findOneById(parseInt(id));;
     }
+
+    @Post()
+    async create(@Body() savePartDto: SavePartDto ) {
+        return await this.partsService.save(savePartDto);
+    }
+
+    @Post("/:part-id/history")
+    async createHistory(
+        @Param('part-id')partId: string, 
+        @Body()saveHistoryDto : SaveHistoryDto) {}
+
+
+    @Patch('/:id')
+    async update(
+        @Param("id") id: string , 
+        @Body() updatePartDto: UpdatePartDto ) {
+        return await this.partsService.modify(parseInt(id), updatePartDto); 
+    }
+
+    @Patch('/:part-id/history/:history-id')
+    async updateHistory(
+        @Param('part-id') partId, 
+        @Param('history-id') historyId ,
+        @Body() historyUpdateDto: UpdateHistoryDto ) {}
+
+    @Delete('/:id')
+    async delete(@Param("id") id: string) {
+        return await this.partsService.remove(parseInt(id));
+    }
+
+    @Delete('/:part-id/history/:history-id')
+    async deleteHistory(
+        @Param('part-id') partId, 
+        @Param('history-id') historyId ,
+        @Body() historyUpdateDto: UpdateHistoryDto ) {}
 }
