aed29730c5615e2df1ed55831189d1a2eabf1bb9
Beomjin <qjawls646@gmail.com>
2024-06-04 05:08:47 +0000

    서비스에 필요한 로직 작성

--- src/parts/parts.service.ts
+++ src/parts/parts.service.ts
@@ -1,29 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository, UpdateResult } from 'typeorm';
 import { Part } from './entities/part.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { SavePartDto } from './dtos/savePart.dto';
+import { History } from 'src/histories/entities/history.entity';
+import { UpdatePartDto } from './dtos/updatePart.dto';
 
 @Injectable()
 export class PartsService {
-    constructor(@InjectRepository(Part) private partRepository: Repository<Part>) {}
+    constructor(
+        @InjectRepository(Part) private partRepository: Repository<Part>, 
+        @InjectRepository(History) private readonly historyRepository: Repository<History>
+    ) {}
 
-    async findAll(): Promise<Part[]> {
+    public findAll(): Promise<Part[]> {
         return this.partRepository.find();
     }
 
-    async findOneById(id: number): Promise<Part> {
-        return this.partRepository.findOne({where: {id}});
+    public async findOneById(id: number): Promise<Part> {
+        const part = await this.identifyExistenceById(id);
+
+        return part
     }
 
-    async save(part: Partial<Part>): Promise<Part> {
-        return this.partRepository.save(part);
+    public save(savePartDto: SavePartDto): Promise<Part> {
+        const {isImport, date, quantity, ...partProperties} = savePartDto;
+        
+        const history = new History({isImport, date, quantity});
+        const part = new Part(partProperties);
+        
+        part.histories = [history];
+
+        return this.partRepository.save(part)
     }
 
-    async remove(part: Part): Promise<Part> {
+    public async remove(id: number): Promise<Part> {
+        const part = await this.identifyExistenceById(id);
+
         return this.partRepository.remove(part);
     }
 
-    async modify(id: number, part: Partial<Part>) {
+    public async modify(id: number, UpdatePartDto: UpdatePartDto):Promise<UpdateResult> {
+        const {number, name, manufacturer, storageLocation, detailedStorageLocation, isImport, date, quantity} = UpdatePartDto;
+        await this.identifyExistenceById(id)
+        
         return this.partRepository.update({id}, part);
+    }
+
+    private async identifyExistenceById(id: number): Promise<Part> {
+        const part = await this.findOneById(id);
+        if (!part) {
+            throw new NotFoundException()
+        }
+        return part
     }
 }
