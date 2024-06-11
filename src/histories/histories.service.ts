f0489283a04c48e346cbcf8b26c1043bb93d08a4
beomjin97 <qjawls646@gmail.com>
2024-06-11 16:28:43 +0900

    identifyExistenceById 에 의한 exception 처리

--- src/histories/histories.service.ts
+++ src/histories/histories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { History } from "./entities/history.entity";
 import { Repository } from "typeorm";
@@ -11,18 +11,24 @@ export class HistoriesService {
         return this.historyRepository.save(new History(history))
     }
 
-    public delete(id:number) {
-        this.historyRepository.delete(id);
+    public async delete(id:number) {
+        await this.identifyExistenceById(id);
+        return this.historyRepository.delete(id);
     }
 
-    public update(id:number, partial: Partial<History>) {
-        this.historyRepository.update(id, partial);
+    public async update(id:number, partial: Partial<History>) {
+        await this.identifyExistenceById(id);
+        return this.historyRepository.update(id, partial);
     }
 
     private async identifyExistenceById(id:number) {
         const history = await this.historyRepository.findOne({
             where: {id}
         })
+
+        if (!history) {
+            throw new NotFoundException('해당 기록이 없습니다.')
+        }
     }
 
 }
