3bfa76eff556af8d73f3b06509eb922095c55536
beomjin97 <qjawls646@gmail.com>
2024-06-11 16:17:37 +0900

    updateHistory와 deleteHistroy 메서드를 history 컨트롤러로 분리

--- src/parts/parts.controller.ts
+++ src/parts/parts.controller.ts
@@ -2,56 +2,45 @@ import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/commo
 import { PartsService } from './parts.service';
 import { SavePartDto } from './dtos/savePart.dto';
 import { UpdatePartDto } from './dtos/updatePart.dto';
-import { SaveHistoryDto } from 'src/histories/dtos/saveHistory.dto';
+import { AddHistoryDto } from 'src/histories/dtos/addHistory.dto';
 import { UpdateHistoryDto } from 'src/histories/dtos/updateHistory.dto';
 
 @Controller('part')
 export class PartsController {
     
-    constructor(private partsService:PartsService) {}
-    
-    @Get()
-    async findAll() {
-        return await this.partsService.findAll();
+  constructor(private partsService:PartsService) {}
+  
+  @Get()
+  async findAll() {
+    return await this.partsService.findAll();
+  }
+
+  @Get("/:id")
+  async findOne(@Param("id") id: string) {
+    return await this.partsService.findOneById(parseInt(id));
+  }
+
+  @Post()
+  async create(@Body() savePartDto: SavePartDto ) {
+    return await this.partsService.save(savePartDto);
+  }
+
+  @Post("/:partId/history")
+  async addHistory(
+    @Param('partId')partId: string, 
+    @Body()addHistoryDto : AddHistoryDto) {
+      return this.partsService.addHistory(parseInt(partId), addHistoryDto);
     }
 
-    @Get("/:id")
-    async findOne(@Param("id") id: string) {
-        return await this.partsService.findOneById(parseInt(id));;
-    }
+  @Patch('/:id')
+  async update(
+    @Param("id") id: string , 
+    @Body() updatePartDto: UpdatePartDto ) {
+      return await this.partsService.modify(parseInt(id), updatePartDto); 
+  }
 
-    @Post()
-    async create(@Body() savePartDto: SavePartDto ) {
-        return await this.partsService.save(savePartDto);
-    }
-
-    @Post("/:part-id/history")
-    async createHistory(
-        @Param('part-id')partId: string, 
-        @Body()saveHistoryDto : SaveHistoryDto) {}
-
-
-    @Patch('/:id')
-    async update(
-        @Param("id") id: string , 
-        @Body() updatePartDto: UpdatePartDto ) {
-        return await this.partsService.modify(parseInt(id), updatePartDto); 
-    }
-
-    @Patch('/:part-id/history/:history-id')
-    async updateHistory(
-        @Param('part-id') partId, 
-        @Param('history-id') historyId ,
-        @Body() historyUpdateDto: UpdateHistoryDto ) {}
-
-    @Delete('/:id')
-    async delete(@Param("id") id: string) {
-        return await this.partsService.remove(parseInt(id));
-    }
-
-    @Delete('/:part-id/history/:history-id')
-    async deleteHistory(
-        @Param('part-id') partId, 
-        @Param('history-id') historyId ,
-        @Body() historyUpdateDto: UpdateHistoryDto ) {}
+  @Delete('/:id')
+  async delete(@Param("id") id: string) {
+    return await this.partsService.remove(parseInt(id));
+  }
 }
