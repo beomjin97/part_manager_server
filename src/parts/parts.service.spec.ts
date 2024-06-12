303689fc96bb61bce231c40153abab5da04ba35f
beomjin97 <qjawls646@gmail.com>
2024-06-12 16:06:48 +0900

    save, findAll, findOneById, modify, addHistory, remove 메서드에 대한 테스트 구현

--- src/parts/parts.service.spec.ts
+++ src/parts/parts.service.spec.ts
@@ -2,11 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PartsService } from './parts.service';
 import { Part } from './entities/part.entity';
 import { TypeOrmModule} from '@nestjs/typeorm';
-import { History } from './entities/history.entity';
+import { SavePartDto } from './dtos/savePart.dto';
+import { AddHistoryDto } from '../histories/dtos/addHistory.dto';
+import { HistoriesModule } from '../histories/histories.module';
+import { History } from '../histories/entities/history.entity';
 
 describe('PartsService', () => {
   let service: PartsService;
-  let part:Part;
+  let savePartDto: SavePartDto
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,20 +21,25 @@ describe('PartsService', () => {
             synchronize: true,
             dropSchema: true,
             }), 
-          TypeOrmModule.forFeature([Part, History])],
+          TypeOrmModule.forFeature([Part]),
+          HistoriesModule
+        ],
         providers: [
           PartsService,
         ],
       }).compile();
 
       service = module.get<PartsService>(PartsService);
-
-      part = new Part ();
-      part.number = "test_number";
-      part.name = "test_name";
-      part.manufacturer = "test_company";
-      part.storageLocation = "test_storageLocation";
-      part.DetailedStorageLocation = "test_DetailedStorageLocation";
+      savePartDto = {
+        number: "test_number",
+        name: "test_name",
+        manufacturer: "test_company",
+        storageLocation: "test_storageLocation",
+        detailedStorageLocation: "test_DetailedStorageLocation",
+        isImport: true,
+        date: new Date("2024-05-04"),
+        quantity: 1    
+      }
   })
 
   it('should be defined', () => {
@@ -39,30 +47,44 @@ describe('PartsService', () => {
   });
 
   it('save', async () => {
-    const result = await service.save(part);
-    expect(result.name).toBe(part.name);
+    const result = await service.save(savePartDto);
+    expect(result.number).toBe(savePartDto.number);
   });
 
   it('findAll', async () => {
     const result = await service.findAll();
-    expect(result).toEqual([part]);
+    expect(result).toBeInstanceOf(Array)
+    expect(result[0].number).toBe(savePartDto.number);
   });
 
-  it('remove',  async () => {
-    const result = await service.remove(part);
-    expect(result).toEqual(part);
-
-    const empty = await service.findAll();
-    expect(empty).toEqual([]);
+  it('findOneById', async () => {
+    const result = await service.findOneById(1);
+    expect(result.number).toBe(savePartDto.number);
   })
 
   it('modify', async () => {
-    const existingEntity = await service.save(part);
-
-    const updatedName = 'updated_name';
-    await service.modify(existingEntity.id, {name: updatedName});
+    const updatedNumber = 'updated_number';
+    await service.modify(1, {number: updatedNumber});
     
-    const updatedEntity = await service.findOneById(existingEntity.id);
-    expect(updatedEntity.name).toBe(updatedName);
+    const updatedEntity = await service.findOneById(1);
+    expect(updatedEntity.number).toBe(updatedNumber);
+  })
+
+  it('addHistory', async () => {
+    const addHistoryDto: AddHistoryDto = {
+      isImport: false,
+      quantity: 1,
+      date: new Date("2024-01-01")
+    }
+
+    const result = await service.addHistory(1, addHistoryDto);
+    expect(result.histories.length).toBe(2);
+    expect(result.histories[1].isImport).toBeFalsy()
+  })
+
+  it('remove',  async () => {
+    await service.remove(1);
+    const empty = await service.findAll();
+    expect(empty).toEqual([]);
   })
 });
