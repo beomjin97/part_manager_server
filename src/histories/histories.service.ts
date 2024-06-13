6bb72e6eaa587a7c90bf4228093e098eb0cfe791
beomjin97 <qjawls646@gmail.com>
2024-06-13 14:22:41 +0900

    create, update 메서드의 두번째 파라미터의 타입을 dto로 변경

--- src/histories/histories.service.ts
+++ src/histories/histories.service.ts
@@ -2,33 +2,35 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { History } from "./entities/history.entity";
 import { Repository } from "typeorm";
+import { AddHistoryDto } from "./dtos/addHistory.dto";
+import { UpdateHistoryDto } from "./dtos/updateHistory.dto";
 
 @Injectable()
 export class HistoriesService {
-    constructor(@InjectRepository(History) private readonly historyRepository : Repository<History> ) {}
+	constructor(@InjectRepository(History) private readonly historyRepository : Repository<History> ) {}
 
-    public create(history: Partial<History>):Promise<History> {
-        return this.historyRepository.save(new History(history))
-    }
+	public create(addHistoryDto: AddHistoryDto):Promise<History> {
+		return this.historyRepository.save(addHistoryDto)
+	}
 
-    public async delete(id:number) {
-        await this.identifyExistenceById(id);
-        return this.historyRepository.delete(id);
-    }
+	public async delete(id:number) {
+		await this.identifyExistenceById(id);
+		return this.historyRepository.delete(id);
+	}
 
-    public async update(id:number, partial: Partial<History>) {
-        await this.identifyExistenceById(id);
-        return this.historyRepository.update(id, partial);
-    }
+	public async update(id:number, updateHistoryDto: UpdateHistoryDto) {
+		await this.identifyExistenceById(id);
+		return this.historyRepository.update(id, updateHistoryDto);
+	}
 
-    private async identifyExistenceById(id:number) {
-        const history = await this.historyRepository.findOne({
-            where: {id}
-        })
+	private async identifyExistenceById(id:number) {
+		const history = await this.historyRepository.findOne({
+			where: {id}
+		})
 
-        if (!history) {
-            throw new NotFoundException('해당 기록이 없습니다.')
-        }
-    }
+		if (!history) {
+			throw new NotFoundException('해당 기록이 없습니다.')
+		}
+	}
 
 }
